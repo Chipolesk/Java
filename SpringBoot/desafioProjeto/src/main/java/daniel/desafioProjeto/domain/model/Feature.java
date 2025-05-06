ca87cc3854603e2a305a13860193673229beb322
Chipolesk <danielchipolesk@gmail.com>
2025-05-05 23:27:54 -0300

    Implementando a camada de dominio do projeto

---
+++ SpringBoot/desafioProjeto/src/main/java/daniel/desafioProjeto/domain/model/Feature.java
@@ -0,0 +1,12 @@
+package daniel.desafioProjeto.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_features")
+public class Feature extends BaseItem{
+
+
+}
---
+++ SpringBoot/desafioProjeto/src/main/java/daniel/desafioProjeto/domain/model/News.java
@@ -0,0 +1,11 @@
+package daniel.desafioProjeto.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+
+}
