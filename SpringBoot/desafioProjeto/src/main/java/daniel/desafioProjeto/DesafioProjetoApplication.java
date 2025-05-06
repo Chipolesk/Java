d7c2654538fbd4a68e847a39536a413be7d5dec9
Chipolesk <danielchipolesk@gmail.com>
2025-05-06 17:06:27 -0300

    Fix: corrigindo CORS

--- SpringBoot/desafioProjeto/src/main/java/daniel/desafioProjeto/DesafioProjetoApplication.java
+++ SpringBoot/desafioProjeto/src/main/java/daniel/desafioProjeto/DesafioProjetoApplication.java
@@ -1,8 +1,11 @@
 package daniel.desafioProjeto;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "DEFAULT SERVER URL")})
 @SpringBootApplication
 public class DesafioProjetoApplication {
 
