a4c79118303f4ac8fb0e7e5cb39865512b006284
Chipolesk <danielchipolesk@gmail.com>
2025-05-19 20:16:26 -0300

    Design Pattern

---
+++ DesignPatterns/src/org/designPattern/Context.java
@@ -0,0 +1,15 @@
+package org.designPattern;
+
+public class Context {
+    private Strategy strategy;
+
+    public void setStrategy(Strategy strategy){
+        this.strategy = strategy;
+    }
+    public void executeStrategy(){
+        if(strategy != null){
+            strategy.execute();
+        }
+    }
+
+}
